5584c90f5c6bc36d67729b0a573d46282e32e4db
Mohamed Emam <mohamedemam0@outlook.com>
2015-09-23 02:11:11 +0200

    fr

---
+++ Background Task/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Background_Task
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await Register();
+        }
+
+        async Task Register()
+        {
+            await BackgroundExecutionManager.RequestAccessAsync();
+            var builder = new BackgroundTaskBuilder();
+            builder.Name = "MyBackgroundTask";
+            var condition = new SystemCondition(SystemConditionType.InternetAvailable);
+            var trigger = new SystemTrigger(SystemTriggerType.TimeZoneChange, false);
+            builder.TaskEntryPoint = typeof(MyBackgroundTask.TimeZoneTask).FullName;
+            builder.AddCondition(condition);
+            builder.SetTrigger(trigger);
+            builder.Register();
+        }
+    }
+}
---
+++ MyBackgroundTask/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace MyBackgroundTask
+{
+    public sealed class TimeZoneTask: IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            var toastxml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var toastText = toastxml.GetElementsByTagName("text");
+            (toastText[0] as XmlElement).InnerText = "Hello from Background Task";
+            var toast = new ToastNotification(toastxml);
+            toastNotifier.Show(toast);
+        }
+    }
+}
